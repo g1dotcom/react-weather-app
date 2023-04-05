8fa172c7535c48d5f83ed629d66ed99dfbf98088
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-05 19:40:56 +0300

    created weatherService.js

---
+++ src/services/weatherService.js
@@ -0,0 +1,4 @@
+const API_KEY = "3320da48438dec85f7460ff6068efb7b";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3987a208e91c&units=metric
