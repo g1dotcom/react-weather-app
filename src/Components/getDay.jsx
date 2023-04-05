42c38eada937a30a9b2569d68521d7661b324a3c
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-05 16:51:25 +0300

    css design for getDay.jsx

--- src/Components/getDay.jsx
+++ src/Components/getDay.jsx
@@ -3,27 +3,27 @@ import React from "react";
 const getDay = () => {
   return (
     <div className="h-64 mx-10 flex justify-between flex-wrap">
-      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center ">
+      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center shadow-xl shadow-gray-600 ">
         <h1>Monday</h1>
         <h1>Monday</h1>
         <h1>Monday</h1>
       </div>
-      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center ">
+      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center shadow-xl shadow-gray-600">
         <h1>Monday</h1>
         <h1>Monday</h1>
         <h1>Monday</h1>
       </div>
-      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center ">
+      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center shadow-xl shadow-gray-600 ">
         <h1>Monday</h1>
         <h1>Monday</h1>
         <h1>Monday</h1>
       </div>
-      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center ">
+      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center shadow-xl shadow-gray-600">
         <h1>Monday</h1>
         <h1>Monday</h1>
         <h1>Monday</h1>
       </div>
-      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center ">
+      <div className="bg-white w-1/6 p-10 rounded-3xl flex flex-col justify-between items-center shadow-xl shadow-gray-600">
         <h1>Monday</h1>
         <h1>Monday</h1>
         <h1>Monday</h1>
