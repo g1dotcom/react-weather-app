3d0a614e068b63dc841c8a74f4ef73eab41b5764
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-05 20:00:49 +0300

    bug fixed

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -3,8 +3,16 @@ import React from "react";
 //components
 import Left from "../Components/Left";
 import Right from "../Components/Right";
+import getWeatherData from "../services/weatherService";
 
 const Main = () => {
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", { q: "London" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="flex justify-center rounded-3xl  h-full shadow-2xl shadow-slate-600">
       <div className=" w-2/6 rounded-l-3xl bg-white">
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3987a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
-  const url = new URL(BASE_URL + "/" + "infoType");
+  const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
   return fetch(url)
