e76c5fd19ecd8a4c9721b15650be968113b004b7
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-05 19:41:22 +0300

    css design

--- src/Components/Left.jsx
+++ src/Components/Left.jsx
@@ -5,12 +5,12 @@ import CityInput from "./CityInput";
 
 const Left = () => {
   return (
-    <div className="h-full w-full">
-      <div className="h-1/6 flex items-center justify-center  p-5">
+    <div className="h-full w-full rounded-3xl">
+      <div className="h-1/6 flex items-center justify-center rounded-3xl  p-5">
         <CityInput />
       </div>
       <div className="h-4/6 bg-red-200"></div>
-      <div className="h-1/6 bg-amber-300"></div>
+      <div className="h-1/6 rounded-3xl  "></div>
     </div>
   );
 };
--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -6,7 +6,7 @@ import Right from "../Components/Right";
 
 const Main = () => {
   return (
-    <div className="flex justify-center rounded-3xl  h-full">
+    <div className="flex justify-center rounded-3xl  h-full shadow-2xl shadow-slate-600">
       <div className=" w-2/6 rounded-l-3xl bg-white">
         <Left />
       </div>
