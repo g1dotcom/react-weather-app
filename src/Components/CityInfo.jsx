1aa4a162ba08f808877422bd6564b160c5ef77d2
Gökhan Süle <thegkhnsl@gmail.com>
2023-04-07 20:10:05 +0300

    added css animation

--- src/Components/CityInfo.jsx
+++ src/Components/CityInfo.jsx
@@ -9,15 +9,25 @@ const CityInfo = ({
   return (
     <div className="w-full h-full">
       <div className="flex flex-col justify-between items-center">
-        <img
-          className="w-60 h-60"
-          src="https://freepngimg.com/thumb/weather/23525-5-weather-free-download.png"
-          alt=""
-        />
+        {" "}
+        <div className="weather-card sunny  mt-10 bg-gray-500 ">
+          <div className="sun "></div>
+          <div className="cloud">
+            <div className="animasyon my-4 mx-4"></div>
+            <div className="animasyon my-8 mx-8"></div>
+            <div className="animasyon my-12 mx-12"></div>
+            <div className="animasyon my-8 mx-16"></div>
+          </div>
+          <div className="info font-main">
+            <h2>Sunny</h2>
+            <p>It's a beautiful day.</p>
+            <h1 className="text-2xl font-medium font-main font-mono">
+              <span className="text-lg font-main"> {name}</span>{" "}
+              {`${temp.toFixed()}`}°c
+            </h1>
+          </div>
+        </div>
         <div className="flex flex-col   w-full p-5">
-          <h1 className="text-6xl font-bold font-mono">
-            {`${temp.toFixed()}`}°c
-          </h1>
           <h1 className="text-lg  font-medium font-main my-5">
             {formatToLocalTime(dt, timezone)}{" "}
           </h1>
@@ -33,11 +43,11 @@ const CityInfo = ({
           </p>
           <div className=" flex justify-center items-center w-full truncate">
             <img
-              className="relative bg-cover w-full mt-10 h-32 rounded-3xl opacity-50"
+              className="relative bg-cover w-full mt-10 h-32 rounded-3xl "
               src="https://i.pinimg.com/600x315/66/81/ee/6681eea42fc9c35376a8171f0f82e97f.jpg"
               alt=""
             />
-            <h1 className="absolute text-gray-500 text-4xl font-semibold truncate">
+            <h1 className="absolute mb-10 text-gray-500 text-2xl font-semibold truncate">
               {name},{country}
             </h1>
           </div>
